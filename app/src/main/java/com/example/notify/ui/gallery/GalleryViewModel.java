ff89a3c9ad08431afe4b24570506541d4e4a9922
Sathwik <sathwikgowrishetty99@gmail.com>
2021-10-03 18:37:19 +0530

    added logout feature!!

--- app/src/main/java/com/example/notify/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/example/notify/ui/gallery/GalleryViewModel.java
@@ -10,7 +10,7 @@ public class GalleryViewModel extends ViewModel {
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("You have no notifications for now!!!");
     }
 
     public LiveData<String> getText() {
--- app/src/main/java/com/example/notify/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/notify/ui/slideshow/SlideshowFragment.java
@@ -1,9 +1,12 @@
 package com.example.notify.ui.slideshow;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,16 +15,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.notify.MainActivity;
 import com.example.notify.R;
 import com.example.notify.databinding.FragmentSlideshowBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
     private FragmentSlideshowBinding binding;
-
+    Activity context;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        context = getActivity();
         slideshowViewModel =
                 new ViewModelProvider(this).get(SlideshowViewModel.class);
 
@@ -35,7 +41,21 @@ public class SlideshowFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
         return root;
+    }
+
+    public void onStart(){
+        super.onStart();
+        Button btn = (Button) context.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(context,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
